e74d18d0b4027b1908cda065773e4448e82d4af8
Matt Pazar <mattpazar@yahoo.com>
2019-02-26 14:20:17 -0600

    async, noscript, meta theme color

--- app.js
+++ app.js
@@ -21,4 +21,11 @@
             }
         }  
     }
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('sw.js')
+            .then(function() { console.log("Service Worker Registered"); });
+    }
 }());
+
+
